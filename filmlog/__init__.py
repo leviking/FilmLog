5528f834cf44ea764ca2652bdb609a857d356d19
Tim Soderstrom <tim@moocowproductions.org>
2018-06-09 17:00:18 -0500

    Error handling

--- filmlog/__init__.py
+++ filmlog/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import abort, redirect
+from flask import abort, redirect, render_template
 from sqlalchemy.dialects.mysql import INTEGER, SMALLINT
 from datetime import date
 from flask_login import LoginManager, UserMixin
@@ -40,6 +40,22 @@ csrf = CSRFProtect(app)
 # Views
 from filmlog import views
 
+# Error Handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    app.logger.error('Server Error: %s', (error))
+    return render_template('errors/500.html'), 500
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', (e))
+    return render_template('errors/500.html'), 500
+
+# Template Filters
 @app.template_filter('to_date')
 def _jinja2_filter_date(date, fmt=None):
     if date is None:
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . venv/bin/activate
 export FLASK_APP="filmlog"
-export FLASK_DEBUG=1
+export FLASK_DEBUG=0
 cd filmlog
 flask run --host=0.0.0.0
