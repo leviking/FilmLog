d98c573cd40575901d6df70d97b32712a5923e3a
Tim Soderstrom <tim@moocowproductions.org>
2019-06-10 18:26:41 -0500

    Added config.py (include restructuring)

---
+++ filmlog/config.py
@@ -0,0 +1,30 @@
+import ConfigParser
+import os
+import re
+
+from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+from filmlog import database
+
+app = Flask('filmlog')
+engine = database.engine
+
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../config.ini'))
+
+app.secret_key = config.get('session', 'secret_key')
+app.config['TESTING'] = config.getboolean('app', 'testing')
+app.debug = config.getboolean('app', 'debug')
+
+user_files = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                          config.get('files', 'user_files'))
+
+app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = user_files
+app.config['THUMBNAIL_SIZE'] = config.get('files', 'thumbnail_size')
+
+app.config['RECAPTCHA_PUBLIC_KEY'] = config.get('recaptcha', 'public_key')
+app.config['RECAPTCHA_PRIVATE_KEY'] = config.get('recaptcha', 'private_key')
+
+# Global CSRF Protection
+csrf = CSRFProtect(app)
