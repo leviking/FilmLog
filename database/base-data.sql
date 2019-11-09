6bb85318f641b9c8271c11d8ccad9fd9b0339954
Tim Soderstrom <tim@moocowproductions.org>
2019-11-08 18:00:42 -0600

    Add Ortho Plus

--- database/base-data.sql
+++ database/base-data.sql
@@ -82,6 +82,7 @@ VALUES (1,1,'Ektar',100,'Color Negative'),
 (45,14,'X FILM', 80, 'Black & White Negative'),
 (46,6,'Silvermax',100,'Black & White Negative'),
 (47,6,'HR-50',50,'Black & White Negative');
+(48,3,'Ortho Plus',80,'Black & White Negative');
 
 INSERT INTO PaperFilters (name) VALUES ('00');
 INSERT INTO PaperFilters (name) VALUES ('0');
---
+++ database/migrations/20191108.sql
@@ -0,0 +1,1 @@
+INSERT INTO FilmTypes VALUES (48,3,'Ortho Plus',80,'Black & White Negative');
