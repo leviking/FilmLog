daef237901953a32722f4e7bdca47e561ac591d3
Tim Soderstrom <tim@moocowproductions.org>
2020-01-22 17:42:05 -0600

    Adding Ortho Litho film

--- database/base-data.sql
+++ database/base-data.sql
@@ -83,7 +83,8 @@ VALUES (1,1,'Ektar',100,'Color Negative'),
 (46,6,'Silvermax',100,'Black & White Negative'),
 (47,6,'HR-50',50,'Black & White Negative'),
 (48,3,'Ortho Plus',80,'Black & White Negative'),
-(49,2,'Pro 400H', 400, 'Color Negative');
+(49,2,'Pro 400H', 400, 'Color Negative'),
+(50,13,'Ortho Litho', 10, 'Black & White Negative');
 
 
 INSERT INTO PaperFilters (name) VALUES ('00');
---
+++ database/migrations/20200122.sql
@@ -0,0 +1,1 @@
+INSERT INTO FilmTypes VALUES (50,13,'Ortho Litho', 10, 'Black & White Negative');
