8a4ce8a8a50d6bbfa4e48192ea6b0204c06403ef
Tim Soderstrom <tim@moocowproductions.org>
2020-02-15 21:34:51 -0600

    Lint fixins

--- filmlog/static/js/user.js
+++ filmlog/static/js/user.js
@@ -8,39 +8,38 @@ function getPreferences() {
     dataType: 'json',
     success(data) {
       let preferences = '<tr id="autoUpdateFilmStock">';
-        preferences += '<td><strong>Auto Update Film Stock</strong></td>';
-        preferences += `<td><select id="autoUpdateFilmStockSelect" onchange='changeAutoUpdateFilmStock()'>`;
-        if (data.autoUpdateFilmStock === 'Yes') {
-          preferences += '<option value="No">No</option>';
-          preferences += '<option value="Yes" selected>Yes</option>';
-        } else {
-          preferences += '<option value="No" selected>No</option>';
-          preferences += '<option value="Yes">Yes</option>';
-        }
-        preferences += '</select></td></tr>';
-        $('#preferencesTableBody').append($(preferences));
-    }
+      preferences += '<td><strong>Auto Update Film Stock</strong></td>';
+      preferences += '<td><select id="autoUpdateFilmStockSelect" onchange=\'changeAutoUpdateFilmStock()\'>';
+      if (data.autoUpdateFilmStock === 'Yes') {
+        preferences += '<option value="No">No</option>';
+        preferences += '<option value="Yes" selected>Yes</option>';
+      } else {
+        preferences += '<option value="No" selected>No</option>';
+        preferences += '<option value="Yes">Yes</option>';
+      }
+      preferences += '</select></td></tr>';
+      $('#preferencesTableBody').append($(preferences));
+    },
   });
 }
 
+// Called from HTML up above
+// eslint-disable-next-line no-unused-vars
 function changeAutoUpdateFilmStock() {
-  // alert($('#autoUpdateFilmStockSelect').val());
-    const data = {
-      data: {
-        name: 'autoUpdateFilmStock',
-        value: $('#autoUpdateFilmStockSelect').val()
-      }
-    };
+  const data = {
+    data: {
+      name: 'autoUpdateFilmStock',
+      value: $('#autoUpdateFilmStockSelect').val(),
+    },
+  };
   jQuery.ajax({
     type: 'PATCH',
-    url: `/api/v1/user/preferences`,
+    url: '/api/v1/user/preferences',
     data: JSON.stringify(data),
     contentType: 'application/json',
-    dataType: 'json'
-    // success: updateHolderState(holderID, state),
+    dataType: 'json',
   });
 }
-
 
 $(document).ready(() => { getPreferences(); });
 
