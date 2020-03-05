99d057088ab64edaa05089f0933ccd2d0cc50bd5
Tim Soderstrom <tim@moocowproductions.org>
2020-03-04 18:15:58 -0600

    Fixed FK changes during migration

--- database/migrations/20200303.sql
+++ database/migrations/20200303.sql
@@ -19,4 +19,24 @@ SELECT 1, filmTypeID, CONCAT(brand, ' ', name), iso, kind
 FROM GlobalFilmTypes
 JOIN GlobalFilmBrands ON GlobalFilmBrands.filmBrandID = GlobalFilmTypes.filmBrandID;
 
+ALTER TABLE FilmStock
+  DROP FOREIGN KEY FilmStock_filmTypeID_fk,
+  ADD CONSTRAINT FilmStock_FilmTypes_fk FOREIGN KEY (userID, filmTypeID) REFERENCES FilmTypes (userID, filmTypeID) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE Films
+  DROP FOREIGN KEY Films_filmTypeID_fk,
+  ADD CONSTRAINT Films_FilmTypes_fk FOREIGN KEY (userID, filmTypeID) REFERENCES FilmTypes (userID, filmTypeID) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE Holders
+  DROP FOREIGN KEY Holders_filmTypeID,
+  ADD CONSTRAINT Holders_FilmTypes_fk FOREIGN KEY (userID, filmTypeID) REFERENCES FilmTypes (userID, filmTypeID) ON UPDATE CASCADE;
+
+ALTER TABLE Exposures
+  DROP FOREIGN KEY Exposures_filmTypeID_fk,
+  ADD CONSTRAINT Exposures_FilmTypes_fk FOREIGN KEY (userID, filmTypeID) REFERENCES FilmTypes (userID, filmTypeID) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE MaxBlackTests
+  DROP FOREIGN KEY mbt_filmTypes,
+  ADD CONSTRAINT mbt_filmTypes FOREIGN KEY (userID, filmTypeID) REFERENCES FilmTypes (userID, filmTypeID);
+
 SET FOREIGN_KEY_CHECKS = 1;
