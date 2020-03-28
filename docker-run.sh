6d37245453d3412cd6b2f2b64e8426d4c723d009
Tim Soderstrom <tim@moocowproductions.org>
2020-03-28 13:18:05 -0500

    Update docker to Python 3, prep for Nomad

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,11 @@
+# Dev
+#docker -D run \
+#-t -i \
+#-v /home/tim/git/filmlog/filmlog:/srv/filmlog.org/filmlog \
+#-p 5000:5000 \
+#filmlog
+
+# Prod
 docker -D run \
 -t -i \
--v /home/tim/git/filmlog/filmlog:/srv/filmlog.org/filmlog \
--p 5000:5000 \
-filmlog
-#-p 5000:80 filmlog
-
+-p 5000:80 filmlog
